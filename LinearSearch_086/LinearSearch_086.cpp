3eceb6aca3342cd2245fbac50fa0591578e1c224
ImamDwiYanPermana <imampermana224@gmail.com>
2023-04-14 15:20:29 +0700

    Memasukan input dengan prosedur while true dan if artinya jika n lebih kecil dari 0 dan n lebih kecil sama dengan 20 makan berhenti

--- LinearSearch_086/LinearSearch_086.cpp
+++ LinearSearch_086/LinearSearch_086.cpp
@@ -5,5 +5,15 @@ int arr[20]; // Array to be searched
 int n; // Number of elements in the array
 int i; // Index of array element
 
+void input()
+{
+    while (true) {
+        cout << "enter teh number of ekements int the array: ";
+        cin >> n;
+        if ((n > 0) && (n <= 20))
+            break;
+        else
+            cout << "\nArray SHould have minimum 1 and maximum 20 element.\n\n";
+    }
 
 
